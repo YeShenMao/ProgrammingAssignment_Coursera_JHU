02640c20586907680bcaf213b57ff56d4aac4353
YeShenMao <167003117+YeShenMao@users.noreply.github.com>
2024-04-16 15:16:32 +0800

    Add files via upload

---
+++ JHUCoursera_homework_2.R
@@ -0,0 +1,28 @@
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+## 这个函数创建了一个特殊的列表对象，它可以存储一个矩阵和它的逆。这个对象包含了设置和获取矩阵及其逆的函数。
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+}
